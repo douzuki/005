5fd490f444710306ed008e9c224a51759bf1ba85
douzuki <141471283+douzuki@users.noreply.github.com>
2023-08-06 05:05:13 +0000

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,247 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "   example.org   https:\/\/example.org   copyright  "
+},
+{
+  "id": "arithmetic-and-absolute-value",
+  "level": "1",
+  "url": "arithmetic-and-absolute-value.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Arithmetic and Absolute Value",
+  "body": " Arithmetic and Absolute Value  Absolute value If is a real number, the absolute value (or magnitude ) of , denoted , is the distance between and zero on the number line. If and are real numbers, the absolute value of their difference, denoted , is the distance between and on the number line.  Adding numbers with like signs To add two real numbers with like signs (both positive or both negative), add their magnitudes (absolute values), then prefix their common sign.  Adding numbers with unlike signs To add two real numbers with unlike signs (one positive and one negative), subtract the number with the smaller magnitude (absolute value) from the number with the larger magnitude, then prefix the sign of the number with the larger magnitude.  Subtracting Subtracting is identical to adding the opposite of :  Multiplying and dividing numbers with like signs The product and quotient of two real numbers with like signs are positive.  Multiplying and dividing numbers with unlike signs The product and quotient of two real numbers with unlike signs are negative.  Exponents If is a real number and is a whole number, then  Odd or even exponents A negative real number raised to an even exponent is positive, while a negative real number raised to an odd exponent is negative.  "
+},
+{
+  "id": "p-1",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-1",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Absolute value "
+},
+{
+  "id": "p-2",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Adding numbers with like signs "
+},
+{
+  "id": "p-3",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Adding numbers with unlike signs "
+},
+{
+  "id": "p-4",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-4",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Subtracting "
+},
+{
+  "id": "p-5",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-5",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Multiplying and dividing numbers with like signs "
+},
+{
+  "id": "p-6",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-6",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Multiplying and dividing numbers with unlike signs "
+},
+{
+  "id": "p-7",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-7",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Exponents "
+},
+{
+  "id": "p-8",
+  "level": "2",
+  "url": "arithmetic-and-absolute-value.html#p-8",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Odd or even exponents "
+},
+{
+  "id": "order-of-operations",
+  "level": "1",
+  "url": "order-of-operations.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Order of Operations",
+  "body": " Order of Operations  Order of operations When evaluating expressions, proceed in the following order.  Evaluate expressions contained in parentheses first. If parentheses are nested, evaluate the expression in the innermost pair of parentheses first.  Evaluate all exponents that appear in the expression.  Perform all multiplications and divisions in the order that they appear in the expression, moving left to right  Perform all additions and subtractions in the order that they appear in the expression, moving left to right.    "
+},
+{
+  "id": "p-9",
+  "level": "2",
+  "url": "order-of-operations.html#p-9",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Order of operations "
+},
+{
+  "id": "rational-number",
+  "level": "1",
+  "url": "rational-number.html",
+  "type": "Section",
+  "number": "1.3",
+  "title": "Rational Numbers",
+  "body": " Rational Numbers  Rational numbers Any number that can be expressed in the form , where and are integers, , is called a rational number . The set of rational numbers is denoted by .  Lowest Terms A fraction is said to be reduced or in lowest terms if and only if and .  Multiplying Fractions Simply multiply numerators and multiply denominators: Canceling a factor in the numerator for an identical factor in the denominator may simplify the calculation.  Dividing Fractions Dividing by a number is the same as multiplying by its reciprocal:  Adding\/Subtracting Fractions If two fractions have a denominator in common, add\/subtract the numerators and place the result over the common denominator: If the fractions do not share a common denominator, rewrite the fractions so that they do.  "
+},
+{
+  "id": "p-10",
+  "level": "2",
+  "url": "rational-number.html#p-10",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Rational numbers "
+},
+{
+  "id": "p-11",
+  "level": "2",
+  "url": "rational-number.html#p-11",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Lowest Terms "
+},
+{
+  "id": "p-12",
+  "level": "2",
+  "url": "rational-number.html#p-12",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Multiplying Fractions "
+},
+{
+  "id": "p-13",
+  "level": "2",
+  "url": "rational-number.html#p-13",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Dividing Fractions "
+},
+{
+  "id": "p-14",
+  "level": "2",
+  "url": "rational-number.html#p-14",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Adding\/Subtracting Fractions "
+},
+{
+  "id": "algebraic-expressions",
+  "level": "1",
+  "url": "algebraic-expressions.html",
+  "type": "Section",
+  "number": "1.4",
+  "title": "Algebraic Expressions",
+  "body": " Algebraic Expressions  Term A term is either a single number (called a constant term ) or the product of a number and one or more variables. The number is called the coefficient of the term.  Combining like terms Two terms are called like terms if they have identical variable parts, which means that the terms must contain the same variables raised to the same exponents. To add\/subtract like terms, add\/subtract their coefficients and keep the same variable parts.  Distributive property If , , and are numbers, then  Simplified polynomial A polynomial is simplified if the terms are written in decreasing or increasing order of their powers of and no unnecessary parentheses, coefficients, signs, or powers are used.  "
+},
+{
+  "id": "p-15",
+  "level": "2",
+  "url": "algebraic-expressions.html#p-15",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Term "
+},
+{
+  "id": "p-16",
+  "level": "2",
+  "url": "algebraic-expressions.html#p-16",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Combining like terms "
+},
+{
+  "id": "p-17",
+  "level": "2",
+  "url": "algebraic-expressions.html#p-17",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Distributive property "
+},
+{
+  "id": "p-18",
+  "level": "2",
+  "url": "algebraic-expressions.html#p-18",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Simplified polynomial "
+},
+{
+  "id": "linear-equations",
+  "level": "1",
+  "url": "linear-equations.html",
+  "type": "Section",
+  "number": "2.1",
+  "title": "Linear Equations",
+  "body": " Linear Equations  Linear  "
+},
+{
+  "id": "p-19",
+  "level": "2",
+  "url": "linear-equations.html#p-19",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Linear "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
